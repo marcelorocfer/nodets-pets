26ed1776c942619182ae7b06fee6b4779caa779f
Marcelo <marceloengecomp@gmail.com>
2023-01-12 15:50:09 -0300

    Base project created

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response) => {
+    res.send('home controller');
+}
+export const dogs = (req: Request, res: Response) => {
+
+}
+export const cats = (req: Request, res: Response) => {
+
+}
+export const fish = (req: Request, res: Response) => {
+
+}
